ed11706f97ad817a60413bc0aa3790afdeb6d603
Gurpreet Gill <gurpreetgill42@gmail.com>
2015-05-24 12:06:11 +0530

    Initial Commit

---
+++ FFTimage/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FFTimage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.uploadImageButton = new System.Windows.Forms.Button();
+            this.uploadedPictureBox = new System.Windows.Forms.PictureBox();
+            this.fftPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.uploadedPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fftPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // uploadImageButton
+            // 
+            this.uploadImageButton.Location = new System.Drawing.Point(321, 600);
+            this.uploadImageButton.Name = "uploadImageButton";
+            this.uploadImageButton.Size = new System.Drawing.Size(424, 61);
+            this.uploadImageButton.TabIndex = 0;
+            this.uploadImageButton.Text = "Upload Image";
+            this.uploadImageButton.UseVisualStyleBackColor = true;
+            this.uploadImageButton.Click += new System.EventHandler(this.uploadImageButton_Click);
+            // 
+            // uploadedPictureBox
+            // 
+            this.uploadedPictureBox.Location = new System.Drawing.Point(12, 55);
+            this.uploadedPictureBox.Name = "uploadedPictureBox";
+            this.uploadedPictureBox.Size = new System.Drawing.Size(512, 512);
+            this.uploadedPictureBox.TabIndex = 1;
+            this.uploadedPictureBox.TabStop = false;
+            // 
+            // fftPictureBox
+            // 
+            this.fftPictureBox.Location = new System.Drawing.Point(531, 55);
+            this.fftPictureBox.Name = "fftPictureBox";
+            this.fftPictureBox.Size = new System.Drawing.Size(512, 512);
+            this.fftPictureBox.TabIndex = 2;
+            this.fftPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(209, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(752, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "FFT Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 673);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fftPictureBox);
+            this.Controls.Add(this.uploadedPictureBox);
+            this.Controls.Add(this.uploadImageButton);
+            this.Name = "Form1";
+            this.Text = "fftImage Coverter";
+            ((System.ComponentModel.ISupportInitialize)(this.uploadedPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fftPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button uploadImageButton;
+        private System.Windows.Forms.PictureBox uploadedPictureBox;
+        private System.Windows.Forms.PictureBox fftPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FFTimage/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using AForge.Imaging;
+namespace FFTimage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        Bitmap output;
+
+        private Bitmap ColorToGrayscale(Bitmap bmp)
+        {
+            int w = bmp.Width,
+            h = bmp.Height,
+            r, ic, oc, bmpStride, outputStride, bytesPerPixel;
+            PixelFormat pfIn = bmp.PixelFormat;
+            ColorPalette palette;
+            BitmapData bmpData, outputData;
+
+            //Create the new bitmap
+            output = new Bitmap(w, h, PixelFormat.Format8bppIndexed);
+
+            //Build a grayscale color Palette
+            palette = output.Palette;
+            for (int i = 0; i < 256; i++)
+            {
+                Color tmp = Color.FromArgb(255, i, i, i);
+                palette.Entries[i] = Color.FromArgb(255, i, i, i);
+            }
+            output.Palette = palette;
+
+            //No need to convert formats if already in 8 bit
+            if (pfIn == PixelFormat.Format8bppIndexed)
+            {
+                output = (Bitmap)bmp.Clone();
+
+                //Make sure the palette is a grayscale palette and not some other
+                //8-bit indexed palette
+                output.Palette = palette;
+
+                return output;
+            }
+
+            //Get the number of bytes per pixel
+            switch (pfIn)
+            {
+                case PixelFormat.Format24bppRgb: bytesPerPixel = 3; break;
+                case PixelFormat.Format32bppArgb: bytesPerPixel = 4; break;
+                case PixelFormat.Format32bppRgb: bytesPerPixel = 4; break;
+                default: throw new InvalidOperationException("Image format not supported");
+            }
+
+            //Lock the images
+            bmpData = bmp.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly,
+            pfIn);
+            outputData = output.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.WriteOnly,
+            PixelFormat.Format8bppIndexed);
+            bmpStride = bmpData.Stride;
+            outputStride = outputData.Stride;
+
+            //Traverse each pixel of the image
+            unsafe
+            {
+                byte* bmpPtr = (byte*)bmpData.Scan0.ToPointer(),
+                outputPtr = (byte*)outputData.Scan0.ToPointer();
+
+                if (bytesPerPixel == 3)
+                {
+                    //Convert the pixel to it's luminance using the formula:
+                    // L = .299*R + .587*G + .114*B
+                    //Note that ic is the input column and oc is the output column
+                    for (r = 0; r < h; r++)
+                        for (ic = oc = 0; oc < w; ic += 3, ++oc)
+                            outputPtr[r * outputStride + oc] = (byte)(int)
+                            (0.299f * bmpPtr[r * bmpStride + ic] +
+                            0.587f * bmpPtr[r * bmpStride + ic + 1] +
+                            0.114f * bmpPtr[r * bmpStride + ic + 2]);
+                }
+                else //bytesPerPixel == 4
+                {
+                    //Convert the pixel to it's luminance using the formula:
+                    // L = alpha * (.299*R + .587*G + .114*B)
+                    //Note that ic is the input column and oc is the output column
+                    for (r = 0; r < h; r++)
+                        for (ic = oc = 0; oc < w; ic += 4, ++oc)
+                            outputPtr[r * outputStride + oc] = (byte)(int)
+                            ((bmpPtr[r * bmpStride + ic] / 255.0f) *
+                            (0.299f * bmpPtr[r * bmpStride + ic + 1] +
+                            0.587f * bmpPtr[r * bmpStride + ic + 2] +
+                            0.114f * bmpPtr[r * bmpStride + ic + 3]));
+                }
+            }
+
+            //Unlock the images
+            bmp.UnlockBits(bmpData);
+            output.UnlockBits(outputData);
+
+            return output;
+        }
+
+
+        private void uploadImageButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap inputImage = new Bitmap(openFileDialog1.FileName);
+                System.Drawing.Image sizeImage = (System.Drawing.Image)( new Bitmap(inputImage, 512,512));
+                inputImage = (Bitmap)(sizeImage);
+                uploadedPictureBox.Image = inputImage;
+                inputImage = ColorToGrayscale(inputImage);
+                ComplexImage finalImg = ComplexImage.FromBitmap(inputImage);
+                finalImg.ForwardFourierTransform();
+                Bitmap outputImg = finalImg.ToBitmap();
+                fftPictureBox.Image = outputImg;
+                inputImage.Dispose();
+                output.Dispose();
+            }
+        }
+    }
+}
